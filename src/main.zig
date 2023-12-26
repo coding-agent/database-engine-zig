7ea018721d310717e8425b3b192f0d1ec5d052bc
Marcius Silva <marciusdasilva51@gmail.com>
2023-12-26 12:01:26 +0000

    Update: Implemented process arguments

--- src/main.zig
+++ src/main.zig
@@ -1,5 +1,10 @@
 const std = @import("std");
+var gpa = std.heap.GeneralPurposeAllocator(.{}){};
 
 pub fn main() !void {
+    const args_allocator = gpa.allocator();
+    const args = try std.process.argsAlloc(args_allocator);
+    defer std.process.argsFree(args_allocator, args);
 
+    std.debug.print("", .{});
 }
