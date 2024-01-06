e6e4acbc31a69bbd594d2355e685546cb58966d6
Marcius Silva <marciusdasilva51@gmail.com>
2024-01-06 19:48:21 +0000

    restarting project

--- src/file_management/Types.zig
+++
@@ -1,8 +0,0 @@
-const std = @import("std");
-const Dir = std.fs.Dir;
-const File = std.fs.File;
-
-pub const DatabaseFileOptions = struct{
-    path:?Dir,
-    file:?File
-};
--- src/main.zig
+++ src/main.zig
@@ -1,17 +1,6 @@
 const std = @import("std");
-const interpreter = @import("./interpreter.zig").interpreter;
-const Parser = @import("./parser/parser.zig");
-const Arena = std.heap.ArenaAllocator;
 
 pub fn main() !void {
-    var arena = Arena.init(std.heap.page_allocator);
-    const allocator = arena.allocator();
-    const args = try std.process.argsAlloc(allocator);
-    const input = args[1..];
-    defer arena.deinit();
-
-    const parsed_input = try Parser.parse(input);
-    try interpreter(parsed_input, allocator, .{});
 }
 
 test "input" {
